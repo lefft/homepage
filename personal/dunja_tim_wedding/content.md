045d7c3d9153d6e56b96e2afb1eb9941e93dd639
lefft <tjleffel@gmail.com>
2018-04-17 00:46:37 -0500

    live version going up

---
+++ personal/dunja_tim_wedding/content.md
@@ -0,0 +1,21 @@
+
+
+
+TODO:
+  - colors on rsvp form [dootza]
+  - finalize rsvp form content [timi]
+  - shape/color/etc of dropdowns [timi]
+  - make invites [timi+dootza]
+  - send invites [?]
+
+
+
+
+rsvp items:
+  x- are you coming? 
+  x- how many ppl?
+  x- name(s)
+  x- children?
+  x- note
+
+
---
+++ personal/dunja_tim_wedding/scripts/expandall.js
@@ -0,0 +1,24 @@
+// NOT WORKING
+// <input id="clickMe" type="button" value="clickme" onclick="doFunction();" />
+// <button onclick="doFunction();" id="EXPANDALL">BLAHHH</div>
+
+
+
+// function toggleAll() {
+//     var buttons = Array.prototype.slice.call(document.querySelectorAll('.clsPromptButton'));
+
+//     buttons.forEach(function (button) {
+//         // Swap each element between display 'none' and 'block'
+//         if (window.toggleAll === 0) {
+//             button.style.display = 'block';
+//         } else {
+//             button.style.display = 'none';
+//         }
+//         // Swap window.toggleAll between 0 and 1
+//         window.toggleAll = window.toggleAll === 0 ? 1 : 0;
+//     });
+// }
+
+
+// document.getElementById("EXPANDALL").onclick = toggleAll();
+
